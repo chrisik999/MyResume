abc3c4bb306e2af8cf821d3f54a816972856b04d
chrisik999 <christianikechukwu505@gmail.com>
2021-08-19 13:52:27 +0100

    fix: refactor error  to Heroku config var

--- app.js
+++ app.js
@@ -16,15 +16,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 console.log(path.join(__dirname, 'public'))
 app.post("/contact", (req, res) => {
     const user = req.body;
-    console.log(user);
+    console.log(user)
     User.create({
         name: user.name,
         email: user.email,
         message: user.message
     }, (err, newUser) => {
-        if (err) return res.status(500).json({message: "not Successful",error: err});
-        else {
+        if (err){
+            console.log(err);
+        return res.status(500).json({message: "not Successful",error: err});
+        
+        } else {
             mail(req);
+            console.log(response);
             return res.status(200).json({message: "success", user: newUser});
         }
     })
